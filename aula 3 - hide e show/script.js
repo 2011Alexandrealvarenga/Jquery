43746723af00bfd0c369f3a6456a5c94660f65a3
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-03 20:54:12 -0300

    oculta e exibe

---
+++ aula 3 - hide e show/script.js
@@ -0,0 +1,15 @@
+// ao iniciar a pagina
+$(document).ready(iniciar);
+
+function iniciar(){
+
+    // evento ao clicar (click)
+    // hide oculta
+    $("div p").hide();
+    $("span").click(function(){
+        // show mostra
+        $("div p").show();
+    })
+
+    
+}
