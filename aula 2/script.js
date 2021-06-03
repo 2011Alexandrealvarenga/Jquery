e5d94c6904fced15bdfdcd0615e69bee98a2249d
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-03 19:33:41 -0300

    alterando prorpiedade de id

---
+++ aula 2/script.js
@@ -0,0 +1,10 @@
+// ao iniciar a pagina
+$(document).ready(iniciar);
+
+function iniciar(){
+    // altera a cor do p
+    $("p").css("color","red");   
+    // alterando propriedade do ID
+    $("#id1").css("background","red")
+             .css("color","white");
+}
