4cd633a062dfa20acfbbafc0ecdca6614785663d
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-03 21:25:15 -0300

    altera classe excetyo 2

--- aula 6/script.js
+++ aula 6/script.js
@@ -2,9 +2,13 @@
 $(document).ready(iniciar);
 
 function iniciar(){
+
     // altera todos elementos exceto com id p1
 $("div p:not(#p1)").css("background","yellow")
+
 // altera todos exceto com a classe
 $("div p:not(.classe1").css("background","green")
+
+$("div p.outra").css("background","lightblue")
 
 }
