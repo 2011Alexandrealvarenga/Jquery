bb1fbb9b0993cc7cf6361002b960ceedc9ea6d91
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-03 21:00:04 -0300

    alterado o ultimo item

---
+++ aula 4/script.js
@@ -0,0 +1,11 @@
+// ao iniciar a pagina
+$(document).ready(iniciar);
+
+function iniciar(){
+// altera o primeiro item da lista
+$("ul li:first").html("alterado").css("background","red"); 
+
+// alterado no final
+$("ul li:last").html("alterado no final")
+                .css("background","yellow");   
+}
