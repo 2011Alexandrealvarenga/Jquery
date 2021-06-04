9a8b068958393b4017b2ca467aabc9b52869fc06
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-03 21:02:27 -0300

    alterado todos os itens

--- aula 4/script.js
+++ aula 4/script.js
@@ -3,9 +3,14 @@ $(document).ready(iniciar);
 
 function iniciar(){
 // altera o primeiro item da lista
-$("ul li:first").html("alterado").css("background","red"); 
+$("ul li:first").html("alterado")
+                .css("background","red"); 
 
 // alterado no final
 $("ul li:last").html("alterado no final")
-                .css("background","yellow");   
+                .css("background","yellow"); 
+
+// alterar todos os itens  
+// $("ul li").css("background","blue");
+
 }
