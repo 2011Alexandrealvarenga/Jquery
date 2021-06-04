e95c97992e0025f8fa8b4b7e74b6573a9f914fea
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-04 16:52:46 -0300

    altera diferente do class

--- aula 8/script.js
+++ aula 8/script.js
@@ -1,5 +1,8 @@
 $(document).ready(iniciar);
 function iniciar(){
     // altera somente o texto com o name especifico
-    $("p[name='texto']").css("background","yellow")
+    $("p[name='texto']").css("background","yellow");
+
+    // altera diferente do class 
+    $("p[class!='texto']").css("background","lightblue")
 }
