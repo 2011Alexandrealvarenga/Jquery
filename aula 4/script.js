4d49f5a7626232b27277050119d30d3321b82aa0
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-03 21:06:19 -0300

    altera texto do priemri o item

--- aula 4/script.js
+++ aula 4/script.js
@@ -2,6 +2,10 @@
 $(document).ready(iniciar);
 
 function iniciar(){
+
+// altera texto do primeiro item
+// $("ul li:first").html("alterados")
+
 // altera o primeiro item da lista
 $("ul li:first").html("alterado")
                 .css("background","red"); 
@@ -12,5 +16,8 @@ $("ul li:last").html("alterado no final")
 
 // alterar todos os itens  
 // $("ul li").css("background","blue");
+
+// altera os elementos pares
+// $("ul li:even").css("background","blue");
 
 }
