d9c9964ccc8b7c336078e9b1bc5a72e1fa6f90de
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-03 21:10:32 -0300

    altera o item maior que

--- aula 5/script.js
+++ aula 5/script.js
@@ -3,6 +3,9 @@ $(document).ready(iniciar);
 
 function iniciar(){
     // altera o item 2 da lista
-$("ol li:eq(2)").css("background","yellow");
+// $("ol li:eq(2)").css("background","yellow");
+
+// maior que
+$("ol li:gt(2)").css("background","yellow");
 
 }
