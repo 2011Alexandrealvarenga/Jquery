aa19d32005f4a8f455e333279a5b20ec0d914719
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-06 12:19:58 -0300

    todos os proximos serao marcados

--- aula 10/script.js
+++ aula 10/script.js
@@ -2,5 +2,9 @@ $(document).ready(iniciar);
 
 function iniciar(){
     // o paragrafo posterior ao id teste sera marcado
-    $('p#teste').next().css('background','yellow')
+    // $('p#teste').next().css('background','yellow');
+
+    // todos os proximos serao marcados
+    $('p#teste').nextAll().css('background','yellow');
+
 }
