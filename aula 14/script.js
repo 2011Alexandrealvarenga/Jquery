e8feeba31fcb47b298c098660f619b8257b9d0d5
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-06 13:40:39 -0300

    slide up - recua para cima

---
+++ aula 14/script.js
@@ -0,0 +1,11 @@
+$(document).ready(iniciar);
+
+function iniciar(){
+    
+    // clica e mostra o alert
+    $('#p1').click( function(){
+        // recua para cima
+        $('#p2').slideUp();
+    })
+
+};
