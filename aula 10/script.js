e91689ef55712ac043e26b0c6e1ab7693db4d22c
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-06 12:21:22 -0300

    // seleciona todos os anteriores ao id teste

--- aula 10/script.js
+++ aula 10/script.js
@@ -5,6 +5,8 @@ function iniciar(){
     // $('p#teste').next().css('background','yellow');
 
     // todos os proximos serao marcados
-    $('p#teste').nextAll().css('background','yellow');
+    // $('p#teste').nextAll().css('background','yellow');
+
+    $('p#teste').prevAll().css('background','yellow');
 
 }
