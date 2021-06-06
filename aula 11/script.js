48bbdc3926d2f8b5ccf0ba3a1465c49871e9b3d8
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-06 12:34:45 -0300

    altera todos exceto o marcado slice

--- aula 11/script.js
+++ aula 11/script.js
@@ -2,6 +2,9 @@ $(document).ready(iniciar);
 
 function iniciar(){
     // todos exceto o id4
-$("p#id4").siblings().css('background','yellow');
+// $("p#id4").siblings().css('background','yellow');
+
+// seleciona de ate
+$("p").slice(1,3).css('background','yellow');
 
 }
