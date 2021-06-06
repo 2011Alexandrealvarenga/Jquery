323c530632db76ec68f631cea0dd07c595c66b56
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-06 11:44:41 -0300

    altera com id e name iguais

--- aula 9/script.js
+++ aula 9/script.js
@@ -1,11 +1,5 @@
 $(document).ready(iniciar);
 function iniciar(){
-    // altera somente o texto com o name especifico
-    $("p[name='texto']").css("background","yellow");
-
-    // altera diferente do class 
-    $("p[class!='texto']").css("background","lightblue")
-
-    // altera classes que comecam com j
-    $("[class^='j']").css("background","pink")
+    // altera com id e name iguais
+    $('div p[id="texto"][name="ole"]').css("background","yellow");
 }
