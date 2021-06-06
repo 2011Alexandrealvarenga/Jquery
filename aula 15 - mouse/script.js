24147715939f0a980aeb9750082d37a8ef346777
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-06 14:07:10 -0300

    mouse move - acrescenta valor ao mover o mouse

--- aula 15 - mouse/script.js
+++ aula 15 - mouse/script.js
@@ -1,14 +1,26 @@
 $(document).ready(iniciar);
-
+var numero = 0;
 function iniciar(){
     // chamar o executar
     // $('#p1').hover(executar);
 
     // quando o mouse entra
-    $('#p1').mouseenter(executar);
+    // $('#p1').mouseenter(executar);
 
+    // quando o mouse sai
+    // $('#p1').mouseleave(executar);
+    
+    // mover o mouse - chama a funcao mouse move
+    $('#p1').mousemove(mousemove);
 
 };
 function executar(){
     $('#p2').toggle();
+}
+function mousemove(){
+    // altera o texto
+    // $('#p2').html("moveu o mouse");
+
+    // acrescenta valor ao mover o mouse
+    $('#p2').text(numero++);
 }
