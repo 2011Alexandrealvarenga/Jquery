b72ecc832ab08be5b777e2a8d55632dc19d11589
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-06 13:52:45 -0300

    slide up - recua para cima com 2s

--- aula 14/script.js
+++ aula 14/script.js
@@ -4,13 +4,16 @@ function iniciar(){
     
     // clica e mostra o alert
     $('#p1').click( function(){
+    // effeito passar o mouse
+    // $('#p1').hover( function(){
         // recua para cima
-        $('#p2').slideUp();
+        // $('#p2').slideUp();
 
         // efeito de 2s
-        $('#p2').slideUp(2000);
+        // $('#p2').slideUp(2000);
 
-        // $('#p2').slideDown();
+        // mostra e esconde
+        $('#p2').toggle();
     })
 
 };
