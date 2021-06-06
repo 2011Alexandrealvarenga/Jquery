03a50d00a76870b748b187bd251f021c650b464c
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-06 13:25:29 -0300

    altera o span apos o id

--- aula 12/script.js
+++ aula 12/script.js
@@ -1,6 +1,9 @@
 $(document).ready(iniciar);
 
 function iniciar(){
-    // altera o id a apos o b
-    $('p#b + p#a').css('background','yellow')
+    // altera o id a apos o
+    // $('p#b + p#a').css('background','yellow')
+
+    // altera o span apos o id b
+    $('p#b ~ span').css('background','yellow')
 }
