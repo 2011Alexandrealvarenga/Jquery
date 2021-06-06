4563ee3a02adfb25ddb02f0c58aee4293efa642a
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-06 13:49:28 -0300

    slide up - recua para cima com 2s

--- aula 14/script.js
+++ aula 14/script.js
@@ -6,6 +6,11 @@ function iniciar(){
     $('#p1').click( function(){
         // recua para cima
         $('#p2').slideUp();
+
+        // efeito de 2s
+        $('#p2').slideUp(2000);
+
+        // $('#p2').slideDown();
     })
 
 };
