accd1fb8a1a9d5342f56aa8c46aa3c4fa07f46c7
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-06-23 14:47:20 -0300

    pressiona uma tecla e retorna o numero no asc

--- aula 24/script.js
+++ aula 24/script.js
@@ -3,13 +3,18 @@ $(document).ready(iniciar);
 function iniciar(){
 
    $('#formulario').submit(function(){return false;})
-   
+
    // pressiona uma tecla e exibe no id
-   $('input:text').keypress(
-      function(){
-         $('#info').text('pressionou um tecla');
-      }
-   )
+   // $('input:text').keypress(
+   //    function(){
+   //       $('#info').text('pressionou um tecla');
+   //    }
+   // )
+
+   // retorna o codigo asc da tecla pressionada
+   $('input:text').bind("keypress", function(e){
+      $('#info').text('tecla pressionada ' + e.which);
+   })   
 }
 
 
