2ad8eeeb69a1f03b8b365a605c66028634bc3a8e
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-07-11 12:42:22 -0300

    add conteudo dentro do conteudo e remove no mesmo click

--- aula 43/script.js
+++ aula 43/script.js
@@ -1,8 +1,14 @@
 $(document).ready(iniciar);
 
  function iniciar(){
-   $('button').click(function(){
-    //  wrap add elemento
-    $('.primeira').wrap('<div class="segunda" />');
-   });
+  //  $('button').click(function(){
+  //   //  wrap add elemento
+  //   $('.primeira').wrap('<div class="segunda" />');
+  //  });
+
+   $('button').toggle(
+     function(){$('.primeira').wrap('<div class="segunda" />');},
+     function(){$('.primeira').unwrap();}
+
+   );   
 }
