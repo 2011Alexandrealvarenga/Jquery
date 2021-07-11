0366257197d3fdea52c83478d2c08e6529648795
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-07-11 11:26:15 -0300

    pega o elemnto e exibe no console

---
+++ aula 39/script.js
@@ -0,0 +1,10 @@
+$(document).ready(iniciar);
+
+ function iniciar(){
+   //  pega todos os elementos
+   // var elementos = $('p').get();
+
+   // pega o elemento 1
+   var elementos = $('p').get(1);
+   console.log(elementos);
+}
