82b2d9ad6097c189df4ee88444c9d68f39fe0391
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-07-11 11:02:07 -0300

    atribuicao de valores 2

---
+++ aula 37/script.js
@@ -0,0 +1,14 @@
+$(document).ready(iniciar);
+var num = 0;
+
+ function iniciar(){
+   // atribuir um evento click ao botao
+   $('button').click(function(){
+
+      // buscar os elementos 'p'
+      $('p').each(function(){
+         // atribui o test 'o novo valor é'
+         $(this).text('o novo valor é:' + num++);
+      });
+   })
+}
