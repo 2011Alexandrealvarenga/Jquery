501d765a2270ef6b0454c4d85617f18bcc395434
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-07-11 12:00:29 -0300

    adicionar classe e verificar se tem

--- aula 41/script.js
+++ aula 41/script.js
@@ -3,11 +3,20 @@ $(document).ready(iniciar);
  function iniciar(){
   $('p').click(function(){
     // add cor no primeiro item da lista
-    $('ul li:first').addClass('cor1');
+    // $('ul li:first').addClass('cor1');
+    // $('ul li:first').addClass('cor1 fundo');
 
     // add cor ao item equivalente
-    $('ul li:eq(2)').addClass('cor1');
+    // $('ul li:eq(2)').addClass('cor1');
 
+    // verificar se elemento html tem a classe
+    $('p').click(function(){
+      var contem = $('li:first').hasClass('cor1');
+      if(contem)
+        alert('sim');
+      else
+        alert('nao');
+    });
 
   })
 }
