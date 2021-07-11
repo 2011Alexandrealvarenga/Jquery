1f1d42dce7d05c4859e20c0c937e732eb60b1b14
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-07-11 19:23:14 -0300

    retorna tag e html

--- aula 47/script.js
+++ aula 47/script.js
@@ -3,9 +3,13 @@ $(document).ready(iniciar);
  function iniciar(){
   $('button').click(function(){
     // html retorna a tag
-    alert($("div").html());
-// text retorna o texto dentro da tag
-    alert($("div").text());
+    // alert($("div").html());
+
+    // text retorna o texto dentro da tag
+    // alert($("div").text());
+
+    // add tag
+    $('.primeiradiv').html('<p>add tag</p>');
 
   })
  
