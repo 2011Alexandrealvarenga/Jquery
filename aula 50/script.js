adbc80fb3083e15fb60523c1db16b65e8e16d789
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-07-11 20:34:47 -0300

    add elimina e remove

--- aula 50/script.js
+++ aula 50/script.js
@@ -2,8 +2,14 @@ $(document).ready(iniciar);
 
  function iniciar(){
 $('button').click(function(){
+  // retira o item li
+  // $('li').detach();
 
+  // limpa o conteudo das li´s
+  // $('li').empty();
 
+  // remove
+  $('li').remove();
 })
  
 }
