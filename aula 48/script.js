fe3408d84d44329dff7e6d7061fcaa61fcd72fcb
2011alexandrealvarenga <2011brunofernandes@gmail.com>
2021-07-11 19:31:39 -0300

    add tag antes prepend to

--- aula 48/script.js
+++ aula 48/script.js
@@ -6,7 +6,9 @@ $('button').click(function(){
   // $('p').append('<p>item 2</p>')
 
   // add antes
-  $('p').prepend('<p>item 3</p>');
+  // $('p').prepend('<p>item 3</p>');
+
+  $('<p>prepend to</p>').prependTo('p');
 })
  
 }
