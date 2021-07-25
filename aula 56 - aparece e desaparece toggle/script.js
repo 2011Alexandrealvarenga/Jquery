f4f7fbc97a317c44810e676ff116e64532d436e0
2011alexandrealvarenga <rgr.alexandre@tokiomarine.com.br>
2021-07-25 18:07:01 -0300

    clica e aparece e desaparece

---
+++ aula 56 - aparece e desaparece toggle/script.js
@@ -0,0 +1,12 @@
+$(document).ready(iniciar);
+
+ function iniciar(){
+$('button').click(function(){
+  // aparece desaparece
+  // $('#animar').toggle();
+
+  // efeito com delay
+  $('#animar').toggle(1000);
+});
+ 
+}
