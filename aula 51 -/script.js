255bcbbf30c9379e7bf78d03a6b21c99073cd6e4
2011alexandrealvarenga <rgr.alexandre@tokiomarine.com.br>
2021-07-25 14:33:18 -0300

    substitui a tag ao clicar

---
+++ aula 51 -/script.js
@@ -0,0 +1,15 @@
+$(document).ready(iniciar);
+
+ function iniciar(){
+$('button').click(function(){
+  // retira o item li
+  // $('li').detach();
+
+  // limpa o conteudo das li´s
+  // $('li').empty();
+
+  // remove
+  $('li').remove();
+})
+ 
+}
